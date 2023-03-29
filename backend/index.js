72512ae3b90249197c1b2abe0aee70d9863d4e05
“Khadis <khadis.aliev@gmail.com>
2023-03-29 12:38:59 -0400

    project setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+
+express()
+  .use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("tiny"))
+  .use(express.static("./server/assets"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use("/", express.static(__dirname + "/"))
+  // add extra endpoints here 
+
+  .listen(8050, () => console.info(`Listening on port 8050`));
